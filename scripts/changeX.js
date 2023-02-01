300a3fd320a0394850d27421ddbe21ab4e810fcd
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-01 21:06:14 +0000

    Added some informative comments

--- scripts/changeX.js
+++ scripts/changeX.js
@@ -6,7 +6,7 @@ async function main() {
 	//get the abi and bytecode of the contract and fill in CONTRACT_ADDRESS
 	const contract = await hre.ethers.getContractAt("Contract", CONTRACT_ADDRESS);
 
-	const tx = await contract.changeX(41);
+	const tx = await contract.changeX(41); //BigNumber { value: "41" }
 	await tx.wait(); //wait for the transaction to be mined
 
 	console.log(`Contract was deployed to ${contract.address}`);
