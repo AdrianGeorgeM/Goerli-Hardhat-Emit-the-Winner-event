1384194f6ab0234191106f34c4905df6b86ff2d3
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-01 20:50:34 +0000

    Create the changeX script to get the abi and bytecode of the contract

---
+++ scripts/changeX.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+const CONTRACT_ADDRESS = "";
+
+async function main() {
+	//get the abi and bytecode of the contract and fill in CONTRACT_ADDRESS
+	const contract = await hre.ethers.getContractAt("Contract", CONTRACT_ADDRESS);
+
+	await contract.changeX(41);
+
+	console.log(`Contract was deployed to ${contract.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
